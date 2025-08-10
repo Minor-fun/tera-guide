9ee0a0d9cab972cb30d052918dedc041d2d1c91e
hyacinth <45744798+Minor-fun@users.noreply.github.com>
2025-08-10 23:52:44 +0800

    Merge hsdn (#5)
    * Add 2802, 2803, 2804
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Dmitry S <dmitry.s@hsdn.org>

--- guides/2802.js
+++ guides/2802.js
@@ -4,59 +4,48 @@
 // made by MissMeowMeow (Amy)
 
 module.exports = (dispatch, handlers, guide, lang) => {
-
-	let print_stun = true;
-	const { HIGHLIGHT_ITEM } = module.parent.exports.spawn;
-
 	return {
 		"nd-2802-1000": [
 			{ type: "stop_timers" },
 			{ type: "despawn_all" }
 		],
-		"h-2802-1000-90": [{ type: "text", sub_type: "notification", message: "Shield Soon", message_RU: "", message_zh: "即将破盾" }],
-		"h-2802-1000-60": [{ type: "text", sub_type: "notification", message: "Shield Soon", message_RU: "", message_zh: "即将破盾" }],
-		"h-2802-1000-30": [{ type: "text", sub_type: "notification", message: "Shield Soon", message_RU: "", message_zh: "即将破盾" }],
-		"h-2802-1000-8": [{ type: "text", sub_type: "notification", message: "D P S ! ? !", message_RU: "", message_zh: "输出 ! ? !" }],
-		"s-2802-1000-103-0": [{ type: "text", sub_type: "message", message: "Knockdown", message_RU: "", message_zh: "击倒", class_position: "tank" }],
-		"s-2802-1000-105-0": [{ type: "text", sub_type: "message", message: "Multi-hit Stun", message_RU: "", message_zh: "多段击晕", class_position: "tank" }],
-		"s-2802-1000-106-0": [{ type: "text", sub_type: "message", message: "Double Spin", message_RU: "", message_zh: "双旋转" }],
-		"s-2802-1000-107-0": [{ type: "text", sub_type: "message", message: "Tank Buster (Slow)", message_RU: "", message_zh: "破防攻击(慢)", class_position: "tank" }],
-		"s-2802-1000-108-0": [{ type: "text", sub_type: "message", message: "Tank Buster (Fast)", message_RU: "", message_zh: "破防攻击(快)", class_position: "tank" }],
-		"s-2802-1000-109-0": [{ type: "text", sub_type: "message", message: "Jump - Dodge", message_RU: "", message_zh: "跳跃 - 躲避" }],
-		"s-2802-1000-110-0": [{ type: "text", sub_type: "message", message: "Pull (Jump Soon)", message_RU: "", message_zh: "拉人 (即将跳跃)" }],
-		"s-2802-1000-111-0": [{ type: "text", sub_type: "message", message: "Res bait (Stun)", message_RU: "", message_zh: "复活诱饵 (晕眩)" }],
-		"s-2802-1000-112-0": [{ type: "text", sub_type: "message", message: "AOE Soon", message_RU: "", message_zh: "即将AOE" }],
-		"s-2802-1000-113-0": [{ type: "text", sub_type: "message", message: "AOE Stun", message_RU: "", message_zh: "AOE晕眩" }],
+		"h-2802-1000-90": [{ type: "text", sub_type: "notification", message: "Shield Soon", message_RU: "Скоро щит", message_zh: "即将破盾" }],
+		"h-2802-1000-60": [{ type: "text", sub_type: "notification", message: "Shield Soon", message_RU: "Скоро щит", message_zh: "即将破盾" }],
+		"h-2802-1000-30": [{ type: "text", sub_type: "notification", message: "Shield Soon", message_RU: "Скоро щит", message_zh: "即将破盾" }],
+		"h-2802-1000-8": [{ type: "text", sub_type: "notification", message: "D P S ! ? !", message_RU: "ДПС ! ? !", message_zh: "输出 ! ? !" }],
+		"s-2802-1000-103-0": [{ type: "text", sub_type: "message", message: "Knockdown", message_RU: "Опрокид", message_zh: "击倒", class_position: "tank" }],
+		"s-2802-1000-105-0": [{ type: "text", sub_type: "message", message: "Multi-hit Stun", message_RU: "Многократный оглушающий удар", message_zh: "多段击晕", class_position: "tank" }],
+		"s-2802-1000-106-0": [{ type: "text", sub_type: "message", message: "Double Spin", message_RU: "Двойное вращение", message_zh: "双旋转" }],
+		"s-2802-1000-107-0": [{ type: "text", sub_type: "message", message: "Tank Buster (Slow)", message_RU: "Удар в танка (медленный)", message_zh: "破防攻击(慢)", class_position: "tank" }],
+		"s-2802-1000-108-0": [{ type: "text", sub_type: "message", message: "Tank Buster (Fast)", message_RU: "Удар в танка (быстрый)", message_zh: "破防攻击(快)", class_position: "tank" }],
+		"s-2802-1000-109-0": [{ type: "text", sub_type: "message", message: "Jump - Dodge", message_RU: "Прыжок - Эвейд", message_zh: "跳跃 - 躲避" }],
+		"s-2802-1000-110-0": [{ type: "text", sub_type: "message", message: "Pull (Jump Soon)", message_RU: "Притягивание (скоро прыжок)", message_zh: "拉人 (即将跳跃)" }],
+		"s-2802-1000-111-0": [{ type: "text", sub_type: "message", message: "Res bait (Stun)", message_RU: "Приманка для воскрешения (оглушение)", message_zh: "复活诱饵 (晕眩)" }],
+		"s-2802-1000-112-0": [{ type: "text", sub_type: "message", message: "AOE Soon", message_RU: "Скоро AOE", message_zh: "即将AOE" }],
+		"s-2802-1000-113-0": [{ type: "text", sub_type: "message", message: "AOE Stun", message_RU: "AOE оглушение", message_zh: "AOE晕眩" }],
 		"s-2802-1000-301-0": [
-			{ type: "text", sub_type: "message", message: "Shield", message_RU: "", message_zh: "盾牌", class_position: ["dps" , "tank"]},
-			{ type: "text", sub_type: "message", message: "Shield - Plague", message_RU: "", message_zh: "祭祀，解buff", class_position: "priest" },
-			{ type: "text", sub_type: "message", message: "Shield - Regress", message_RU: "", message_zh: "元素，解buff", class_position: "mystic" }
+			{ type: "text", sub_type: "message", message: "Shield", message_RU: "Щит", message_zh: "盾牌", class_position: ["dps", "tank"] },
+			{ type: "text", sub_type: "message", message: "Shield - Plague", message_RU: "Щит - Чума", message_zh: "祭祀，解buff", class_position: "priest" },
+			{ type: "text", sub_type: "message", message: "Shield - Regress", message_RU: "Щит - Регресс", message_zh: "元素，解buff", class_position: "mystic" }
 		],
-
-		"s-2802-1000-303-0": [{ type: "text", sub_type: "message", message: "You Suck!?", message_RU: "", message_zh: "boss暴走" }],
-		"s-2802-1000-304-0": [{ type: "text", sub_type: "message", message: "You Suck!?", message_RU: "", message_zh: "boss暴走" }],
-		"s-2802-1000-305-0": [{ type: "text", sub_type: "message", message: "Monkeys", message_RU: "", message_zh: "全体击飞" }],
-
+		"s-2802-1000-303-0": [{ type: "text", sub_type: "message", message: "You Suck!?", message_RU: "Ты отстой!?", message_zh: "boss暴走" }],
+		"s-2802-1000-304-0": [{ type: "text", sub_type: "message", message: "You Suck!?", message_RU: "Ты отстой!?", message_zh: "boss暴走" }],
+		"s-2802-1000-305-0": [{ type: "text", sub_type: "message", message: "Monkeys", message_RU: "Обезьяны", message_zh: "全体击飞" }],
 		"s-2802-1000-306-0": [
-			{ type: "text", sub_type: "message", message: "Donuts Go Out-In", message_RU: "", message_zh: "甜甜圈 外-内" },
+			{ type: "text", sub_type: "message", message: "Donuts Go Out-In", message_RU: "Пончики наружу-внутрь", message_zh: "甜甜圈 外-内" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 15, 300, 0, 5000] },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 650, 0, 5000] }
 		],
-
 		"s-2802-1000-307-0": [
-			{ type: "text", sub_type: "message", message: "Range Bait (Double Jump)", message_RU: "", message_zh: "远程诱饵 (双跳)" },
+			{ type: "text", sub_type: "message", message: "Range Bait (Double Jump)", message_RU: "Приманка дальнего боя (двойной прыжок)", message_zh: "远程诱饵 (双跳)" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 12, 650, 0, 2500] }
 		],
-		
-		"s-2802-1000-308-0": [{ type: "text", sub_type: "message", message: "AOE Soon", message_RU: "", message_zh: "即将AOE" }],
-		
+		"s-2802-1000-308-0": [{ type: "text", sub_type: "message", message: "AOE Soon", message_RU: "Скоро AOE", message_zh: "即将AOE" }],
 		"s-2802-1000-309-0": [
-			{ type: "text", sub_type: "message", message: "Turn (Dodge)", message_RU: "", message_zh: "转身 (躲避)" },
-			{ type: "spawn", func: "circle", delay: 700, args: [false, 553, 0, 0, null, 800, 0, 3500] },
+			{ type: "text", sub_type: "message", message: "Turn (Dodge)", message_RU: "Поворот (эвейд)", message_zh: "转身 (躲避)" },
+			{ type: "spawn", func: "circle", delay: 700, args: [false, 553, 0, 0, null, 800, 0, 3500] }
 		],
-
-		"s-2802-1000-310-0": [{ type: "text", sub_type: "message", message: "Slow Stomp - Dodge", message_RU: "", message_zh: "慢踩踏 - 躲避" }],
-		"s-2802-1000-313-0": [{ type: "text", sub_type: "message", message: "Wide Area - Dodge", message_RU: "", message_zh: "大范围 - 躲避" }],
-
+		"s-2802-1000-310-0": [{ type: "text", sub_type: "message", message: "Slow Stomp - Dodge", message_RU: "Медленное топтание - Эвейд", message_zh: "慢踩踏 - 躲避" }],
+		"s-2802-1000-313-0": [{ type: "text", sub_type: "message", message: "Wide Area - Dodge", message_RU: "Широкая зона - Эвейд", message_zh: "大范围 - 躲避" }]
 	};
 };
--- guides/2803.js
+++ guides/2803.js
@@ -4,7 +4,7 @@
 
 module.exports = (dispatch, handlers, guide, lang) => {
 
-	let print_stun = true;
+	// let print_stun = true;
 
 	return {
 		"nd-2803-1000": [
@@ -12,39 +12,39 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "despawn_all" }
 		],
 
-		"s-2803-1000-1-0": [{ type: "text", sub_type: "message", message: "Double Base Slash", message_RU: "", message_zh: "双重基础斩击" }],
-		"s-2803-1000-2-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "", message_zh: "旋转" }],
-		"s-2803-1000-3-0": [{  type: "text", class_position: "tank", sub_type: "message", message: "Knock Up", message_RU: "", message_zh: "击飞" }],
-		"s-2803-1000-4-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "", message_zh: "旋转" }],
-		"s-2803-1000-5-0": [{ type: "text", class_position: "tank", sub_type: "message", message: "Stun", message_RU: "", message_zh: "晕眩" }],
-		"s-2803-1000-6-0": [{ type: "text", sub_type: "message", message: "Double Spinning", message_RU: "", message_zh: "双重旋转" }],
+		"s-2803-1000-1-0": [{ type: "text", sub_type: "message", message: "Double Base Slash", message_RU: "Двойной базовый удар", message_zh: "双重基础斩击" }],
+		"s-2803-1000-2-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Вращение", message_zh: "旋转" }],
+		"s-2803-1000-3-0": [{  type: "text", class_position: "tank", sub_type: "message", message: "Knock Up", message_RU: "Подбрасывание", message_zh: "击飞" }],
+		"s-2803-1000-4-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Вращение", message_zh: "旋转" }],
+		"s-2803-1000-5-0": [{ type: "text", class_position: "tank", sub_type: "message", message: "Stun", message_RU: "Оглушение", message_zh: "晕眩" }],
+		"s-2803-1000-6-0": [{ type: "text", sub_type: "message", message: "Double Spinning", message_RU: "Двойное вращение", message_zh: "双重旋转" }],
 		"s-2803-1000-7-0": [
-			{ type: "text", sub_type: "message", message: "Pull + KD", message_RU: "", message_zh: "拉人 + 击倒", class_position: ["tank", "dps"]},
-			{ type: "text", sub_type: "message", message: "Kaia", message_RU: "", message_zh: "套盾", class_position: ["mystic", "priest"]},
+			{ type: "text", sub_type: "message", message: "Pull + KD", message_RU: "Притягивание + Сбивание", message_zh: "拉人 + 击倒", class_position: ["tank", "dps"]},
+			{ type: "text", sub_type: "message", message: "Kaia", message_RU: "Щит", message_zh: "套盾", class_position: ["mystic", "priest"]},
 		],
-		"s-2803-1000-8-0": [{ type: "text", class_position: "tank", sub_type: "message", message: "Tank Buster", message_RU: "", message_zh: "破防攻击" }],
-		"s-2803-1000-9-0": [{ type: "text", sub_type: "message", message: "DODGE", message_RU: "", message_zh: "躲避" }],
-		"s-2803-1000-10-0": [{ type: "text", sub_type: "message", message: "Spinning DODGE", message_RU: "", message_zh: "旋转 躲避" }],
-		//"s-2803-1000-111-0": [{ type: "text", sub_type: "message", message: "Roll Back", message_RU: " " }],
-		//"s-2803-1000-112-0": [{ type: "text", sub_type: "message", message: "Gungnir", message_RU: " " }],
-		"s-2803-1000-113-0": [{ type: "text", sub_type: "message", message: "Overhead Slash", message_RU: "", message_zh: "上劈" }],
-		"s-2803-1000-101-0": [{ type: "text", sub_type: "message", message: "SHEILD", message_RU: "", message_zh: "破盾" }],
-		"s-2803-1000-102-0": [{ type: "text", sub_type: "message", message: "PLAGUE/REGRESS", message_RU: "", message_zh: "解buff" }],
-		//"s-2803-1000-303-0": [{ type: "text", sub_type: "message", message: "Wipe Calamity Strike - (Stage1)", message_RU: " " }],
-		//"s-2803-1000-304-0": [{ type: "text", sub_type: "message", message: "Wipe Calamity Strike - (Stage2)", message_RU: " " }],
-		"s-2803-1000-305-0": [{ type: "text", sub_type: "message", message: "RuneBurst", message_RU: "", message_zh: "符文爆发" }],
-		//"s-2803-1000-306-0": [{ type: "text", sub_type: "message", message: "Ragnarok", message_RU: " " }],
-		//"s-2803-1000-7-0": [{ type: "text", sub_type: "message", message: "Glaive Follow", message_RU: " " }],
-		//"s-2803-1000-308-0": [{ type: "text", sub_type: "message", message: "Shinning Slash", message_RU: " " }],
-		//"s-2803-1000-309-0": [{ type: "text", sub_type: "message", message: "Shinning AOE", message_RU: " " }],
-		//"s-2803-1000-310-0": [{ type: "text", sub_type: "message", message: "Twilight 1", message_RU: " " }],
-		//"s-2803-1000-311-0": [{ type: "text", sub_type: "message", message: "Twilight 2", message_RU: " " }],
-		//"s-2803-1000-312-0": [{ type: "text", sub_type: "message", message: "Twilight 3", message_RU: " " }],
-		//"s-2803-1000-313-0": [{ type: "text", sub_type: "message", message: "Twilight 4", message_RU: " " }],
-		//"s-2803-1000-315-0": [{ type: "text", sub_type: "message", message: "Pizza", message_RU: " " }],
+		"s-2803-1000-8-0": [{ type: "text", class_position: "tank", sub_type: "message", message: "Tank Buster", message_RU: "Удар по танку", message_zh: "破防攻击" }],
+		"s-2803-1000-9-0": [{ type: "text", sub_type: "message", message: "DODGE", message_RU: "Уклонение", message_zh: "躲避" }],
+		"s-2803-1000-10-0": [{ type: "text", sub_type: "message", message: "Spinning DODGE", message_RU: "Вращательное уклонение", message_zh: "旋转 躲避" }],
+		//"s-2803-1000-111-0": [{ type: "text", sub_type: "message", message: "Roll Back", message_RU: "Откат" }],
+		//"s-2803-1000-112-0": [{ type: "text", sub_type: "message", message: "Gungnir", message_RU: "Гунгнир" }],
+		"s-2803-1000-113-0": [{ type: "text", sub_type: "message", message: "Overhead Slash", message_RU: "Удар сверху", message_zh: "上劈" }],
+		"s-2803-1000-101-0": [{ type: "text", sub_type: "message", message: "SHEILD", message_RU: "Щит", message_zh: "破盾" }],
+		"s-2803-1000-102-0": [{ type: "text", sub_type: "message", message: "PLAGUE/REGRESS", message_RU: "Чума/Регресс", message_zh: "解buff" }],
+		//"s-2803-1000-303-0": [{ type: "text", sub_type: "message", message: "Wipe Calamity Strike - (Stage1)", message_RU: "Удар бедствия (Этап 1)" }],
+		//"s-2803-1000-304-0": [{ type: "text", sub_type: "message", message: "Wipe Calamity Strike - (Stage2)", message_RU: "Удар бедствия (Этап 2)" }],
+		"s-2803-1000-305-0": [{ type: "text", sub_type: "message", message: "RuneBurst", message_RU: "Взрыв рун", message_zh: "符文爆发" }],
+		//"s-2803-1000-306-0": [{ type: "text", sub_type: "message", message: "Ragnarok", message_RU: "Рагнарёк" }],
+		//"s-2803-1000-7-0": [{ type: "text", sub_type: "message", message: "Glaive Follow", message_RU: "Удар копьём" }],
+		//"s-2803-1000-308-0": [{ type: "text", sub_type: "message", message: "Shinning Slash", message_RU: "Сияющий удар" }],
+		//"s-2803-1000-309-0": [{ type: "text", sub_type: "message", message: "Shinning AOE", message_RU: "Сияющая зона" }],
+		//"s-2803-1000-310-0": [{ type: "text", sub_type: "message", message: "Twilight 1", message_RU: "Сумерки 1" }],
+		//"s-2803-1000-311-0": [{ type: "text", sub_type: "message", message: "Twilight 2", message_RU: "Сумерки 2" }],
+		//"s-2803-1000-312-0": [{ type: "text", sub_type: "message", message: "Twilight 3", message_RU: "Сумерки 3" }],
+		//"s-2803-1000-313-0": [{ type: "text", sub_type: "message", message: "Twilight 4", message_RU: "Сумерки 4" }],
+		//"s-2803-1000-315-0": [{ type: "text", sub_type: "message", message: "Pizza", message_RU: "Пицца" }],
 
 		/*"s-2803-1000-315-0": [
-			{ type: "text", sub_type: "message", message: "Pizza", message_RU: " " },
+			{ type: "text", sub_type: "message", message: "Pizza", message_RU: "Пицца" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 550, 0, 3000] },
 			{ type: "spawn", func: "marker", args: [false, 80, 300, 0, 3500, true, null] },
 			{ type: "spawn", func: "marker", args: [false, 280, 300, 0, 3500, true, null] },
@@ -52,13 +52,13 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 120, 450, 0, 3500] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 240, 450, 0, 3500] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 300, 450, 0, 3500] },
-			{ type: "text", sub_type: "message", delay: 1700, message: "Dodge!", message_RU: " " }
+			{ type: "text", sub_type: "message", delay: 1700, message: "Dodge!", message_RU: "Уклонение!" }
 		],
 
-		//"s-2803-1000-316-0": [{ type: "text", sub_type: "message", message: "Pizza - (Inverted)", message_RU: " " }],
-		
+		//"s-2803-1000-316-0": [{ type: "text", sub_type: "message", message: "Pizza - (Inverted)", message_RU: "Пицца (Обратная)" }],
+
 		/*"s-2803-1000-316-0": [
-			{ type: "text", sub_type: "message", message: "Pizza - (Inverted)", message_RU: " " },
+			{ type: "text", sub_type: "message", message: "Pizza - (Inverted)", message_RU: "Пицца (Обратная)" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 550, 0, 3000] },
 			{ type: "spawn", func: "marker", args: [false, 80, 300, 0, 3500, true, null] },
 			{ type: "spawn", func: "marker", args: [false, 280, 300, 0, 3500, true, null] },
@@ -66,29 +66,29 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 120, 450, 0, 3500] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 240, 450, 0, 3500] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 300, 450, 0, 3500] },
-			{ type: "text", sub_type: "message", delay: 1700, message: "Dodge!", message_RU: " " }
+			{ type: "text", sub_type: "message", delay: 1700, message: "Dodge!", message_RU: "Уклонение!" }
 		],
 
-		//"s-2803-1000-401-0": [{ type: "text", sub_type: "message", message: "Valk-Wait", message_RU: " " }],
-		//"s-2803-1000-501-0": [{ type: "text", sub_type: "message", message: "Last-Stand-Valk", message_RU: " " }],
-		//"s-2803-1000-1-0": [{ type: "text", sub_type: "message", message: "Double Base Slash", message_RU: " " }],
-		//"s-2803-1000-2-0": [{ type: "text", sub_type: "message", message: "Spin & Slash", message_RU: " " }],
-		//"s-2803-1000-3-0": [{ type: "text", sub_type: "message", message: "Leaping Strike", message_RU: " " }],
-		//"s-2803-1000-2104-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: " " }],
-		//"s-2803-1000-2105-0": [{ type: "text", sub_type: "message", message: "BloodFlower", message_RU: " " }],
-		//"s-2803-1000-2106-0": [{ type: "text", sub_type: "message", message: "Slash & Double Spinning Death", message_RU: " " }],
-		//"s-2803-1000-2107-0": [{ type: "text", sub_type: "message", message: "DreamSlash", message_RU: " " }],
-		//"s-2803-1000-2108-0": [{ type: "text", sub_type: "message", message: "Ground Bash", message_RU: " " }],
-		//"s-2803-1000-2109-0": [{ type: "text", sub_type: "message", message: "Glaive Strike", message_RU: " " }],
-		//"s-2803-1000-2110-0": [{ type: "text", sub_type: "message", message: "Spinning Death 3rd Hit", message_RU: " " }],
-		//"s-2803-1000-2112-0": [{ type: "text", sub_type: "message", message: "Gungnir", message_RU: " " }],
-		//"s-2803-1000-2113-0": [{ type: "text", sub_type: "message", message: "Overhead Slash", message_RU: " " }],
-		//"s-2803-1000-3001-0": [{ type: "text", sub_type: "message", message: "DreamSlash", message_RU: " " }],
-		//"s-2803-1000-3002-0": [{ type: "text", sub_type: "message", message: "RuneBurst", message_RU: " " }],
-		//"s-2803-1000-3003-0": [{ type: "text", sub_type: "message", message: "DreamSlash-Pizza", message_RU: " " }],		
+		//"s-2803-1000-401-0": [{ type: "text", sub_type: "message", message: "Valk-Wait", message_RU: "Валькирия - Ждать" }],
+		//"s-2803-1000-501-0": [{ type: "text", sub_type: "message", message: "Last-Stand-Valk", message_RU: "Последний бой Валькирии" }],
+		//"s-2803-1000-1-0": [{ type: "text", sub_type: "message", message: "Double Base Slash", message_RU: "Двойной базовый удар" }],
+		//"s-2803-1000-2-0": [{ type: "text", sub_type: "message", message: "Spin & Slash", message_RU: "Вращение и удар" }],
+		//"s-2803-1000-3-0": [{ type: "text", sub_type: "message", message: "Leaping Strike", message_RU: "Прыжковый удар" }],
+		//"s-2803-1000-2104-0": [{ type: "text", sub_type: "message", message: "Spin", message_RU: "Вращение" }],
+		//"s-2803-1000-2105-0": [{ type: "text", sub_type: "message", message: "BloodFlower", message_RU: "Кровавый цветок" }],
+		//"s-2803-1000-2106-0": [{ type: "text", sub_type: "message", message: "Slash & Double Spinning Death", message_RU: "Удар и двойная смертельная спираль" }],
+		//"s-2803-1000-2107-0": [{ type: "text", sub_type: "message", message: "DreamSlash", message_RU: "Мечтательный удар" }],
+		//"s-2803-1000-2108-0": [{ type: "text", sub_type: "message", message: "Ground Bash", message_RU: "Удар по земле" }],
+		//"s-2803-1000-2109-0": [{ type: "text", sub_type: "message", message: "Glaive Strike", message_RU: "Удар копьём" }],
+		//"s-2803-1000-2110-0": [{ type: "text", sub_type: "message", message: "Spinning Death 3rd Hit", message_RU: "Третий удар смертельной спирали" }],
+		//"s-2803-1000-2112-0": [{ type: "text", sub_type: "message", message: "Gungnir", message_RU: "Гунгнир" }],
+		//"s-2803-1000-2113-0": [{ type: "text", sub_type: "message", message: "Overhead Slash", message_RU: "Удар сверху" }],
+		//"s-2803-1000-3001-0": [{ type: "text", sub_type: "message", message: "DreamSlash", message_RU: "Мечтательный удар" }],
+		//"s-2803-1000-3002-0": [{ type: "text", sub_type: "message", message: "RuneBurst", message_RU: "Взрыв рун" }],
+		//"s-2803-1000-3003-0": [{ type: "text", sub_type: "message", message: "DreamSlash-Pizza", message_RU: "Мечтательный удар - Пицца" }],
 
-		/*"s-2803-1000-315-0": [
-			{ type: "text", sub_type: "message", message: "Pizza", message_RU: " " },
+		"s-2803-1000-315-0": [
+			{ type: "text", sub_type: "message", message: "Pizza", message_RU: "Пицца" },
 			{ type: "spawn", func: "circle", args: [false, 553, 0, 0, 8, 550, 0, 3000] },
 			{ type: "spawn", func: "marker", args: [false, 80, 300, 0, 3500, true, null] },
 			{ type: "spawn", func: "marker", args: [false, 280, 300, 0, 3500, true, null] },
@@ -96,12 +96,12 @@ module.exports = (dispatch, handlers, guide, lang) => {
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 120, 450, 0, 3500] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 240, 450, 0, 3500] },
 			{ type: "spawn", func: "vector", args: [912, 0, 50, 300, 450, 0, 3500] },
-			{ type: "text", sub_type: "message", delay: 1700, message: "Dodge!", message_RU: " " }
-		],/
+			{ type: "text", sub_type: "message", delay: 1700, message: "Dodge!", message_RU: "Уклонение!" }
+		],
 
 		//"am-3030-1000-99000580": [{ type: "text", sub_type: "message", message: "Hit Thorns", message_RU: "Шипы" }],
 
-		/*"nd-3030-2000": [
+		"nd-3030-2000": [
 			{ type: "stop_timers" },
 			{ type: "despawn_all" }
 		],
